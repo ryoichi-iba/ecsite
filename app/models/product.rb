ae8cb0a8e2fdb0ba9f304b18961a69f6dc03e08d
Ryoichi iba <ryoichiaz18@gmail.com>
2019-07-16 01:22:33 +0900

    Initial commit

---
+++ app/models/product.rb
@@ -0,0 +1,10 @@
+class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :unit, presence: true
+
+
+
+
+end
---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+20.times do
+  Product.create!(
+      name: Faker::Book.title,
+      description: Faker::Lorem.paragraph(2),
+      price: Faker::Number.within(100..100000),
+      unit: %w(yen usd).sample
+  )
+  end
